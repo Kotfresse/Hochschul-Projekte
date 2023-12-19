fbe0d0f49950c8da39c19a0e760886232aca5273
Koti <kotfress@gmail.com>
2023-12-19 10:08:53 +0100

    Started all the for loops at the same time

--- Uebungsaufgaben11/SevenColumnPrimes.java
+++ Uebungsaufgaben11/SevenColumnPrimes.java
@@ -1,5 +1,8 @@
+import java.util.concurrent.ExecutorService;
+
 public class SevenColumnPrimes extends Thread {
     public static void main(String[] args) {
+        
         Primer first = new Primer(2, 1);
         Primer second = new Primer(2, 2);
         Primer third = new Primer(2, 3);
@@ -7,6 +10,7 @@ public class SevenColumnPrimes extends Thread {
         Primer fifth = new Primer(2, 5);
         Primer sixth = new Primer(2, 6);
         Primer seventh = new Primer(2, 7);
+        /*
         for (int i = 3; i <= 7523; first.send(i++))
             ;
         first.send(0);// terminates the thread
@@ -20,7 +24,7 @@ public class SevenColumnPrimes extends Thread {
             ;
         fourth.send(0);// terminates the thread
         for (int i = 3; i <= 7523; fifth.send(i++))
-            ;
+            ;   
         fifth.send(0);// terminates the thread
         for (int i = 3; i <= 7523; sixth.send(i++))
             ;
@@ -28,7 +32,49 @@ public class SevenColumnPrimes extends Thread {
         for (int i = 3; i <= 7523; seventh.send(i++))
             ;
         seventh.send(0);// terminates the thread
+        */
+        Thread firsThread = new Thread(()->{
+            for (int i = 3; i <= 7523; first.send(i++))
+            ;
+        first.send(0);// terminates the thread
+        });
+        Thread secondThread = new Thread(()->{
+            for (int i = 3; i <= 7523; second.send(i++))
+            ;
+        second.send(0);// terminates the thread
+        });
+        Thread thirdThread = new Thread(()->{
+            for (int i = 3; i <= 7523; third.send(i++))
+            ;
+        third.send(0);// terminates the thread
+        });
+        Thread fourthThread = new Thread(()->{
+            for (int i = 3; i <= 7523; fourth.send(i++))
+            ;
+        fourth.send(0);// terminates the thread
+        });
+        Thread fifthThread = new Thread(()->{
+            for (int i = 3; i <= 7523; fifth.send(i++))
+            ;
+        fifth.send(0);// terminates the thread
+        });
+        Thread sixthThread = new Thread(()->{
+            for (int i = 3; i <= 7523; sixth.send(i++))
+            ;
+        sixth.send(0);// terminates the thread
+        });
+        Thread seventhThread = new Thread(()->{
+            for (int i = 3; i <= 7523; seventh.send(i++))
+            ;
+        seventh.send(0);// terminates the thread
+        });
+        firsThread.start();
+        secondThread.start();
+        thirdThread.start();
+        fourthThread.start();
+        fifthThread.start();
+        sixthThread.start();
+        seventhThread.start();
 
     }
-
 }
